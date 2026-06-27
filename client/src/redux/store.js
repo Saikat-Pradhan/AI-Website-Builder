6694d8556687714ce55ae210e6ad32a6f40e6dd7
Saikat Pradhan <saikatpradhan65@gmail.com>
2026-06-28 01:52:27 +0530

    Fix import statement for websiteSlice in store configuration

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from './userSlice.js'
-import websiteSlice from './websiteSlice'
+import websiteSlice from './websiteSlice.js'
 
 export const store =  configureStore({
   reducer: {
